df2a17b470882b7d1bf08dc6b1da6193bf655cef
ADARNUL <obozin1998@mail.ru>
2024-03-14 00:22:48 +0700

    add access rights

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,8 @@ use Model\Post;
 use Model\User;
 use Src\View;
 use Src\Request;
+use Src\Auth\Auth;
+
 class Site
 {
     public function index(Request $request): string
@@ -17,6 +19,27 @@ class Site
    {
        return new View('site.hello', ['message' => 'hello working']);
    }
+
+   public function login(Request $request): string
+    {
+   //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+   //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+   //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }   
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
+    }
+
 
    public function signup(Request $request): string
     {
